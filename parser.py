28a9b7ca8d27c106038c9d4aa35a821fc26c67f5
Eizo <eizoassik@gmail.com>
2014-08-06 15:09:45 +0800

    +comments;there's only parser now

--- parser.py
+++ parser.py
@@ -1,6 +1,13 @@
 #encoding=utf-8
+"""
+The one-pass parser that parsers the control string,
+and generate the finally callable object to format
+the given arguments.
+"""
 
-from itertools import cycle
+from directives import combine_atoms
+from directives import AnyFn, IterFn, WriteFn, KwFn
+
 #############
 # String Contents & Flags
 #############
@@ -8,105 +15,9 @@ CURRENT_ARG_COUNT = 'CURRENT_ARG_COUNT'
 CURRENT_KWARG_COUNT = 'CURRENT_KWARG_COUNT'
 ITER_STACK = 'ITER_STACK'
 COMMON_BUFFER = 'COMMON_BUFFER'
+OPTION_BUFFER = 'OPTION_BUFFER'
 ATOM_DEST = 'ATOM_DEST'
 CASE_SENSITIVE = 'CASE_SENSITIVE'
-
-#############
-# Fn class that handles the real function
-# which will be applied on each args.
-#############
-
-
-class Fn(object):
-    """
-    All *Fn classes should inherit the Fn class directly or not directly
-    Then rewrite its own __call__ method
-    """
-    def __init__(self, fn, index=None, options=None):
-        self.fn = fn
-        self.index = index
-        self.options = options
-
-    def __call__(self, *args, **kwargs):
-        """
-        as the __call__ method of object itself looks like:
-            __call__(self, *args, **kwargs)
-        and I'm not gotta change it, processing `args` needs extra attention.
-        """
-        return self.fn(*args, **kwargs)
-
-
-class AnyFn(Fn):
-    """
-    handle the '~A' directives
-    """
-
-    def __init__(self, fn, index=None, options=None):
-        super().__init__(fn, index, options)
-
-    def __call__(self, *args, **kwargs):
-        if self.index is None:
-            return self.fn(args[0])
-        return self.fn(args[self.index])
-
-
-class WriteFn(AnyFn):
-    """
-    handle the '~W' directives
-    just like AnyFn, but this uses repr() rather than fn to
-    product a string from the given argument.
-    """
-
-    def __init__(self, fn=None, index=None, options=None):
-        super().__init__(fn, index, options)
-
-    def __call__(self, *args, **kwargs):
-        if self.index is None:
-            return repr(args[0])
-        return repr(args[self.index])
-
-
-class KwFn(Fn):
-    # TODO try make this compatible with the origin {key} notations
-    """
-    NOT IMPLEMENTED YET
-    """
-
-    def __init__(self, fn, index=None):
-        super().__init__(fn, index)
-
-    def __call__(self, *args, **kwargs):
-        if self.index:
-            return self.fn(args[0])
-        return self.fn(kwargs[self.index])
-
-
-class IterFn(Fn):
-    def __init__(self, atoms, index):
-        super().__init__(atoms, index)
-
-    def __call__(self, *args, **kwargs):
-        # As all the atoms will be called one by one
-        # the index is no more meaningful
-        # This will use the given *one* arg.
-        for atom in self.fn:
-            if isinstance(atom, (AnyFn, KwFn)):
-                atom.index = None
-        pieces = []
-        # TODO HANDLE kwargs
-        atom_iter = cycle(self.fn)
-        args_iter = iter(args[0])
-        for atom in atom_iter:
-            if isinstance(atom, str):
-                pieces.append(atom)
-            elif isinstance(atom, Fn):
-                try:
-                    pieces.append(atom(next(args_iter)))
-                except StopIteration:
-                    break
-            else:
-                raise TypeError('Except Fn or str. {} get.'.format(type(atom)))
-        return ''.join(pieces)
 
 
 ############
@@ -121,6 +32,7 @@ class Flag(object):
 GO_TO_LAST_ITER = Flag('GO TO LAST ITER')
 GO_UPWARD = Flag('GO UPWARD')
 
+
 ###########
 # The parser
 ###########
@@ -130,8 +42,32 @@ def parse_ctrl(ctrl):
     """
     Here, I'm trying to build a one-pass parser to transform the
     given control string into a function, like:
-        _clformat([*args, [**kwargs]])
+        control_func([*args, [**kwargs]])
     who's finally return value is the formatted sting.
+
+    The parser has 3 man steps in the parser:
+        1) initialize the status, `next_action`, `dest`, etc.
+        2) execute the main loop, as:
+            a. call `next_action` with 2 arguments, the current character
+               of the control string and the status of the parser;
+            b. handle the result of the `next_action`,
+            c. again if the control string is not ended.
+        3) combine the atoms to make the final callable, and return it.
+
+    The `next_action` should ALWAYS return 2 values, retval and
+    the next value of next value. There're 4 possible type of retval:
+
+        1) the retval is None, means the new `next_action` is waiting
+           for more characters of the current dirctive/text now;
+        2) the retval is an Exception, and parse_ctrl will just raise it;
+        3) the retval is an Fn-Object or a str, and the retval will be
+           append to atoms;
+        4) the retval is a parser flag, and the parser will execute some
+           extra operations to change the status of the parser, or change
+           the `dest` to handle loops in the control string.
+
+    Anyway, the `next_action` should NEVER raise a exception, but return
+    it as the retval.
     """
     next_action = read
     atoms = []
@@ -139,6 +75,7 @@ def parse_ctrl(ctrl):
               CURRENT_KWARG_COUNT: 0,
               ITER_STACK: [],
               COMMON_BUFFER: [],
+              OPTION_BUFFER: [],
               ATOM_DEST: atoms,
               CASE_SENSITIVE: True}
     dest = atoms
@@ -158,7 +95,7 @@ def parse_ctrl(ctrl):
                 dest = atoms
             else:
                 dest = status[ITER_STACK][-2]
-            # Now get the last frame of the iter stack
+            # Now get the last frame of the iter-stack
             iter_info = status[ITER_STACK].pop()
             # Reset the arg counter
             status[CURRENT_ARG_COUNT] = iter_info[0]['index'] + 1
@@ -171,10 +108,15 @@ def parse_ctrl(ctrl):
             raise TypeError('Got a {} when parsing control string.'
                             '<class \'Fn\'> excepted.'
                             .format(type(retval)))
-    return _combine_atoms(atoms)
+    return combine_atoms(atoms)
 
 
 def read(c: str, status: dict):
+    """
+    The reader to handle plain text.
+    This function will simply append current character into the
+    status[COMMON_BUFFER], until meet '~'.
+    """
     if c == '~':
         if len(status[COMMON_BUFFER]) > 0:
             _buffer = ''.join(status[COMMON_BUFFER])
@@ -188,26 +130,42 @@ def read(c: str, status: dict):
 
 
 def read_tilde(c: str, status: dict):
+    """
+    When meet tildes, the program comes to a big switch.
+    """
     if not status[CASE_SENSITIVE]:
         c = c.upper()
+    # handle escaped characters
     if c == '~':
         status[COMMON_BUFFER].append('~')
         return None, read
-    elif c == 'A':
-        index = status[CURRENT_ARG_COUNT]
-        status[CURRENT_ARG_COUNT] += 1
-        return AnyFn(fn=lambda x: str(x),
-                     index=index), read
-    elif c == 'W':
-        index = status[CURRENT_ARG_COUNT]
-        status[CURRENT_ARG_COUNT] += 1
-        return WriteFn(index=index), read
     elif c == '%':
         status[COMMON_BUFFER].append('\n')
         return None, read
     elif c == 'T':
         status[COMMON_BUFFER].append('\t')
         return None, read
+    # handle prefixing introductions
+    elif c == ':' or c == '@':
+        status[OPTION_BUFFER].append(c)
+        return None, read_tilde
+    # handle directives
+    elif c == 'A':
+        index = status[CURRENT_ARG_COUNT]
+        status[CURRENT_ARG_COUNT] += 1
+        options = ''.join(status[OPTION_BUFFER])
+        status[OPTION_BUFFER].clear()
+        return AnyFn(fn=lambda x: str(x),
+                     index=index,
+                     options=options), read
+    elif c == 'W':
+        index = status[CURRENT_ARG_COUNT]
+        status[CURRENT_ARG_COUNT] += 1
+        options = ''.join(status[OPTION_BUFFER])
+        status[OPTION_BUFFER].clear()
+        return WriteFn(index=index, options=options), read
+    elif c == '$':
+        pass
     elif c == '{':
         status[ITER_STACK].append([{'index': status[CURRENT_ARG_COUNT],
                                     'depth': len(status[ITER_STACK])}])
@@ -218,51 +176,5 @@ def read_tilde(c: str, status: dict):
     elif c == '}':
         return GO_UPWARD, read
     else:
-        return SyntaxError('clformat cannot parse ~%c.' % c), read
-
-
-#################
-# Below, functions to make a fn
-#################
-
-def _count_atom(atoms: list, find: type):
-    counter = 0
-    for atom in atoms:
-        if isinstance(atom, find):
-            counter += 1
-    return counter
-
-
-def _atom_caller(arg, kwargs):
-    def __atom_caller(atom: Fn):
-        # Both ArgFn and IterFn in this case
-        if isinstance(atom, (AnyFn, WriteFn)):
-            return atom(*arg)
-        elif isinstance(atom, IterFn):
-            return atom(*arg)
-        elif isinstance(atom, KwFn):
-            return atom(**kwargs)
-        elif isinstance(atom, str):
-            return atom
-        else:
-            raise TypeError('Error occurred during combing atoms. '
-                            'ArgFn/KwFn objects excepted, got {}.'
-                            .format(str(type(atom))))
-
-    return __atom_caller
-
-
-def _combine_atoms(atoms: list):
-    _arg_count = _count_atom(atoms, AnyFn) + _count_atom(atoms, IterFn)
-    _kwarg_count = _count_atom(atoms, KwFn)
-
-    def __combine_atoms(*args, **kwargs):
-        if len(args) != _arg_count:
-            raise ValueError("Need {} of args, got {}"
-                             .format(_arg_count, len(args)))
-        if len(kwargs) != _kwarg_count:
-            raise ValueError("Need {} of kwargs, got {}"
-                             .format(_kwarg_count, len(kwargs)))
-        return ''.join(map(_atom_caller(args, kwargs), atoms))
-
-    return __combine_atoms
+        return SyntaxError('clformat cannot parse the directive "~{}{}".'
+                           .format(''.join(status[OPTION_BUFFER]), c)), read
