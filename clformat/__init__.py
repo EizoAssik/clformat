5390747d35833a69bfa26af4cacbf173910e036f
Eizo <eizoassik@gmail.com>
2014-08-06 23:50:12 +0800

    1) move the definition of clformat to __init__.py 2) rm _clformat.py

--- clformat/__init__.py
+++ clformat/__init__.py
@@ -1,3 +1,22 @@
-from ._clformat import clformat as _clf
+from .parser import parse_ctrl
 
-clformat = _clf
+
+def clformat(dest, ctrl_str, *args, **kwargs):
+    """
+    This is the wapper to
+    """
+    _string = _clformat(ctrl_str, *args, **kwargs)
+    if dest is False or dest is None:
+        return _string
+    if dest is True:
+        print(_string)
+        return _string
+
+
+def _clformat(ctrl_str, *args, **kwargs):
+    """
+    Format the string here.
+    """
+    _fn = parse_ctrl(ctrl_str)
+    _string = _fn(*args, **kwargs)
+    return _string
